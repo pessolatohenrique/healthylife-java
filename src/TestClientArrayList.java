557ca3f1a3600f4b8ba6ae4743771e891d5eab14
pessolatohenrique <pessolatohenrique@gmail.com>
2021-01-26 18:35:17 -0200

    Utilizacao de ArrayList para Clients

---
+++ src/TestClientArrayList.java
@@ -0,0 +1,34 @@
+import com.br.companyabc.healthylife.exception.HighHeightException;
+import com.br.companyabc.healthylife.exception.HighWeightException;
+import com.br.companyabc.healthylife.model.Client;
+import com.br.companyabc.healthylife.model.Man;
+import com.br.companyabc.healthylife.model.Women;
+
+import java.util.ArrayList;
+
+public class TestClientArrayList {
+    public static void main(String[] args) {
+        try {
+            Client c1 = new Man("Henrique Pessolato", 64.5, 1.65);
+            Client c2 = new Women("Giulia Figueiredo", 50.0, 1.50);
+            Client c3 = new Women("Carla Peixoto", 100.0, 1.60);
+            Client c4 = new Man("Rodrigo Carlos", -50.0, 3.0);
+
+            ArrayList<Client> clientList = new ArrayList<>();
+            clientList.add(c1);
+            clientList.add(c2);
+            clientList.add(c3);
+            clientList.add(c4);
+            clientList.remove(c4);
+
+            for (Client c : clientList) {
+                c.calculateImc();
+                c.classifyImc();
+                System.out.println(c + " -> " + c.getImcClassification());
+            }
+        } catch (Exception ex) {
+            System.out.println("Ocorreu uma exceção: " + ex.getMessage());
+            System.out.println("Classe de exceção: " + ex.getClass().getName());
+        }
+    }
+}
